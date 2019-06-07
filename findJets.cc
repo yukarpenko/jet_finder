76e22c6c145bef49845b4c8245503d1eb9acd6d0
Iurii Karpenko <karpenko@subatech.in2p3.fr>
2019-06-07 12:06:52 +0200

    full events processing and plotting with ROOT
    * full (hydro+jet) events are read from a directory containing
      ROOT files
    * hadrons/partons from the events are fed directly to FASTJET
    * FASTJET output is used to fill the ROOT histograms which are
      then plotted.

--- findJets.cc
+++ findJets.cc
@@ -8,7 +8,21 @@
 #include <iomanip>
 #include <algorithm>
 #include <cstdio>   // needed for io
+#include <ctime>
 #include "fastjet/ClusterSequence.hh"
+//--- ROOT includes
+#include <TApplication.h>
+#include <TTree.h>
+#include <TChain.h>
+#include <TSystemDirectory.h>
+#include <TList.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TLegend.h>
 
 using namespace std;
 
@@ -16,91 +30,133 @@ void outputJetTotalsWfracs(ofstream& fout, int iEvent, vector<fastjet::PseudoJet
 void outputFullJets(ofstream& fout, int iEvent, vector<fastjet::PseudoJet>& jets);
 void outputFullJets_WTA(ofstream& fout, int iEvent, vector<fastjet::PseudoJet>& jets, double R);
 
-int main(int argc, char* argv[]) {
+void processEvents(TString dirname);
 
- if(argc != 3) {
-  cout << "usage: ./findJets  in_file  out_file\n";
-  return 1;
- }
- ifstream fin(argv[1]);
- if(!fin) {
-  cout << "cannot open input file: " << argv[1] << endl;
-  return 1;
- }
- ofstream fout(argv[2]);
- if(!fout) {
-  cout << "cannot open output file: " << argv[2] << endl;
-  return 1;
- }
- // read in the events
- int evIdCurr = 2147483646;  // a huge number not equal to any real event ID
- int nEvents = -1;
- vector<vector<int> > typeIn, colIn, acolIn, jetIdIn;
- vector<vector<double> > EIn, pxIn, pyIn, pzIn, timeIn;
- const int __Nevents = 200;
- jetIdIn.resize(__Nevents);
- typeIn.resize(__Nevents);
- colIn.resize(__Nevents);
- acolIn.resize(__Nevents);
- EIn.resize(__Nevents);
- pxIn.resize(__Nevents);
- pyIn.resize(__Nevents);
- pzIn.resize(__Nevents);
- timeIn.resize(__Nevents);
- string line;
- while (getline(fin, line)) {  // reading the initial partons, line by line
-  istringstream sline(line);
-  int eventId, jetId, __i, __type, __col, __acol;
-  double __px, __py, __pz, __E, __time;
-  sline >> eventId >> jetId >> __i >> __type >> __col >> __acol
-  >> __E >> __px >> __py >> __pz >> __time;
-  if(eventId!=evIdCurr) nEvents++;
-  evIdCurr = eventId;
-  jetIdIn[nEvents].push_back(jetId);
-  typeIn[nEvents].push_back(__type);
-  colIn[nEvents].push_back(__col);
-  acolIn[nEvents].push_back(__acol);
-  EIn[nEvents].push_back(__E);
-  pxIn[nEvents].push_back(__px);
-  pyIn[nEvents].push_back(__py);
-  pzIn[nEvents].push_back(__pz);
-  timeIn[nEvents].push_back(__time);
- } // end file read
+int main(int argc, char* argv[]) {
+ TApplication theApp("App", &argc, argv);
+ time_t time0 = time(nullptr);
+ processEvents("/dlocal/eposvhlle.out/b0b34_trig50_hybM_NOrec/events/");
+ time_t time1 = time(nullptr);
+ cout << "walltime: " << difftime(time1, time0) << " s.\n";
  //----------------------------------------------------------
- for(int iEvent=0; iEvent<nEvents+1; iEvent++) { // find jets in each event
+ //for(int iEvent=0; iEvent<nEvents+1; iEvent++) { // find jets in each event
+  //vector<fastjet::PseudoJet> input_particles;
+  //input_particles.clear();
+
+    //// input_particles.push_back(fastjet::PseudoJet(px,py,pz,E));
+  //for(int i=0; i<EIn[iEvent].size(); i++) {
+   ////double pt = sqrt(pow(pxIn[iEvent].at(i),2) + pow(pyIn[iEvent].at(i),2));
+   ////if(pt > 0.5)  // no difference for vacuum jets!
+   ////double pmod = sqrt(pow(pxIn[iEvent].at(i),2) + pow(pyIn[iEvent].at(i),2) + 
+   ////  pow(pzIn[iEvent].at(i),2));
+   ////double rap = 0.5*log((pmod+pzIn[iEvent].at(i))/(pmod-pzIn[iEvent].at(i)));
+   ////if(fabs(rap)<1.5)  // no unexpected difference for vacuum jets!
+   //fastjet::PseudoJet particle (pxIn[iEvent].at(i),
+     //pyIn[iEvent].at(i), pzIn[iEvent].at(i), EIn[iEvent].at(i));
+   //particle.set_user_index(jetIdIn[iEvent].at(i));
+   //input_particles.push_back(particle);
+  //}
+  //// create a jet definition:
+  //// a jet algorithm with a given radius parameter
+  //double R = 0.4;
+  //fastjet::JetDefinition jet_def(fastjet::antikt_algorithm, R);
+  //// run the jet clustering with the above jet definition
+  //fastjet::ClusterSequence clust_seq(input_particles, jet_def);
+
+  //// get the resulting jets ordered in pt
+  //double ptmin = 3.0;
+  //vector<fastjet::PseudoJet> inclusive_jets = sorted_by_pt(clust_seq.inclusive_jets(ptmin));
+ 
+  //outputJetTotalsWfracs(fout, iEvent, inclusive_jets);
+  ////outputFullJets(fout, iEvent, inclusive_jets);
+  ////outputFullJets_WTA(fout, iEvent, inclusive_jets, R);
+
+ //} // end event loop
+  theApp.Run();
+  return 0;
+}
+
+
+void processEvents(TString dirname)
+{
+ // loadnig the tree
+ TChain *tree = new TChain("treefin;1");
+ TSystemDirectory dir ("rootfiles",dirname);
+ TList *files = dir.GetListOfFiles();
+ TSystemFile *file;
+ TIter next(files);
+ while ((file=(TSystemFile*)next())) {
+  TString fname = file->GetName();
+  cout << "[" << fname.Data() << "]";
+  if(strstr(fname,".root"))
+   tree->Add(dirname+fname);
+ }
+ cout << endl;
+ // accessing the events
+ const int NP = 50000 ;
+ Int_t id[NP], status[NP] ;
+ Float_t px [NP], py[NP], pz[NP], E[NP] ;
+ Short_t ele[NP] ;
+ Int_t npart ;
+ int nevents = tree->GetEntries() ;
+ tree->SetBranchAddress("px",&px[0]) ;
+ tree->SetBranchAddress("py",&py[0]) ;
+ tree->SetBranchAddress("pz",&pz[0]) ;
+ tree->SetBranchAddress("E",&E[0]) ;
+ tree->SetBranchAddress("id",&id[0]) ;
+ tree->SetBranchAddress("status",&status[0]) ;
+ tree->SetBranchAddress("ele",&ele[0]) ;
+ tree->SetBranchAddress("npart",&npart) ;
+ cout<<"processing, events = "<<nevents<<endl ;
+ TH1F *hpt = new TH1F("hpt", "hpt", 20, 0., 200.);
+ TH1F *hptJet = new TH1F("hptJet", "hptJet", 20, 0., 200.);
+ TH1F *hRho = new TH1F("rhoJet", "rhoJet", 20, 0., 0.6);
+ for(int iev=0; iev<nevents; iev++) { // event loop
+  tree->GetEntry(iev) ;
   vector<fastjet::PseudoJet> input_particles;
   input_particles.clear();
-
-    // input_particles.push_back(fastjet::PseudoJet(px,py,pz,E));
-  for(int i=0; i<EIn[iEvent].size(); i++) {
-   //double pt = sqrt(pow(pxIn[iEvent].at(i),2) + pow(pyIn[iEvent].at(i),2));
-   //if(pt > 0.5)  // no difference for vacuum jets!
-   //double pmod = sqrt(pow(pxIn[iEvent].at(i),2) + pow(pyIn[iEvent].at(i),2) + 
-   //  pow(pzIn[iEvent].at(i),2));
-   //double rap = 0.5*log((pmod+pzIn[iEvent].at(i))/(pmod-pzIn[iEvent].at(i)));
-   //if(fabs(rap)<1.5)  // no unexpected difference for vacuum jets!
-   fastjet::PseudoJet particle (pxIn[iEvent].at(i),
-     pyIn[iEvent].at(i), pzIn[iEvent].at(i), EIn[iEvent].at(i));
-   particle.set_user_index(jetIdIn[iEvent].at(i));
+  for(int ip=0; ip<npart; ip++) { // particle loop
+   double rap = 0.5*log((E[ip]+pz[ip])/(E[ip]-pz[ip]));
+   double pt = sqrt(px[ip]*px[ip]+py[ip]*py[ip]);
+   if(status[ip]==10 && fabs(rap)<1.0) {
+    hpt->Fill(pt);
+   }
+   // filling Fastjet input
+   if(status[ip]<10 && pt>1.5 && fabs(rap)<1.0) {
+   fastjet::PseudoJet particle (px[ip], py[ip], pz[ip], E[ip]);
+   particle.set_user_index(0);
    input_particles.push_back(particle);
-  }
-  // create a jet definition: 
-  // a jet algorithm with a given radius parameter
+   }
+  } // end of particle loop
+  // jet finding
   double R = 0.4;
   fastjet::JetDefinition jet_def(fastjet::antikt_algorithm, R);
   // run the jet clustering with the above jet definition
   fastjet::ClusterSequence clust_seq(input_particles, jet_def);
-
   // get the resulting jets ordered in pt
   double ptmin = 3.0;
-  vector<fastjet::PseudoJet> inclusive_jets = sorted_by_pt(clust_seq.inclusive_jets(ptmin));
- 
-  outputJetTotalsWfracs(fout, iEvent, inclusive_jets);
-  //outputFullJets(fout, iEvent, inclusive_jets);
-  //outputFullJets_WTA(fout, iEvent, inclusive_jets, R);
-
- } // end event loop
-  return 0;
+  vector<fastjet::PseudoJet> jets = sorted_by_pt(clust_seq.inclusive_jets(ptmin));
+  for(unsigned int i = 0; i < jets.size(); i++) {
+   hptJet->Fill(jets[i].perp());
+   // jet structure
+   if(jets[i].perp()>40.0) {
+    vector<fastjet::PseudoJet> constituents = jets[i].constituents();
+    for(fastjet::PseudoJet ptl : constituents) {
+     double phi = ptl.delta_R(jets[i]);
+     hRho->Fill(phi, ptl.perp());
+    }
+   }
+  }
+ } // end of event loop
+ // normalizing histos
+ TCanvas *c1 = new TCanvas("pT","pT") ;
+ hpt->Scale(1./(hpt->GetBinWidth(1)*2. * nevents));
+ hpt->Draw();
+ hptJet->Scale(1./(hptJet->GetBinWidth(1)*2. * nevents));
+ hptJet->Draw("same");
+ TCanvas *c2 = new TCanvas("jet structure","jet structure") ;
+ hRho->Scale(1./(nevents));
+ hRho->Draw();
 }
 
 
